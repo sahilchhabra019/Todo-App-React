76c4a00e6b8b88e7146948078632589896b20960
Sahil Chhabra (Pathways Corporate) <sahil.chhabra@pathways.in>
2021-05-19 10:59:01 +0530

    deleting and set state of form

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Table from './Table'
+import Form from './Form';
+
+class App extends Component {
+
+  state = {
+    characters : []
+  }
+ 
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  }
+
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Hello, Sahil!</h1>
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form />
+      </div>
+    )
+  }
+}
+
+export default App
